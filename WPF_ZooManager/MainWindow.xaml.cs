4c14fd2308485a3954a9bbf59df0e65b82a85a95
Thomas Ngo <tngo0508@gmail.com>
2021-09-12 15:10:37 -0700

    Show associated Data

--- WPF_ZooManager/MainWindow.xaml.cs
+++ WPF_ZooManager/MainWindow.xaml.cs
@@ -50,5 +50,36 @@ namespace WPF_ZooManager {
                 MessageBox.Show(e.ToString());
             }
         }
+
+        private void listAssociatedAnimal_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            ShowAssociatedAnimals();
+        }
+
+        private void ShowAssociatedAnimals() {
+            try {
+                string query = "select * from Animal a inner join ZooAnimal za on a.Id = za.AnimalId where za.ZooId = @ZooId";
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter) {
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                    DataTable animalTable = new DataTable();
+
+                    sqlDataAdapter.Fill(animalTable);
+
+                    listAssociatedAnimal.DisplayMemberPath = "Name";
+                    listAssociatedAnimal.SelectedValuePath = "Id";
+                    listAssociatedAnimal.ItemsSource = animalTable.DefaultView;
+                }
+
+            } catch (Exception e) {
+                MessageBox.Show(e.ToString());
+            }
+        }
     }
 }
